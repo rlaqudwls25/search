5eaf4020fa0d5be69da75c964cf5ed685cf98864
rlaqudwls25 <rlaqudwls157@gmail.com>
2021-03-23 11:19:48 +0900

    초기세팅 완료 및 레이아웃 구현 중

---
+++ src/search/component/Settings.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Dropdown, Menu, Button } from "antd";
+import { SettingFilled } from "@ant-design/icons";
+
+/**
+ *
+ * @param {ojbect} param
+ * @returns {() => void} param.logout
+ */
+const Settings = ({ logout }) => {
+  return (
+    <Dropdown
+      overlay={
+        <Menu>
+          <Menu.Item onClick={logout}>로그아웃</Menu.Item>
+        </Menu>
+      }
+      trigger={["click"]}
+      placement="bottomRight"
+    >
+      <Button shape="circle" icon={<SettingFilled />} />
+    </Dropdown>
+  );
+};
+
+export default Settings;
